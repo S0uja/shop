27b1d66f469633f0c5f98afe82c35f1861cf18e7
S0uja <s0uja.create@gmail.com>
2024-02-23 00:14:21 +0400

    Fix bugs

--- server/routes/reviewRouter.js
+++ server/routes/reviewRouter.js
@@ -3,18 +3,18 @@ const router = new Router()
 const ReviewController = require('../controllers/reviewController')
 const authMiddleWare = require('../middleware/authMiddleware')
 
-router.post('/admin', authMiddleWare, ReviewController.create)
-router.post('/user', authMiddleWare, ReviewController.create)
+router.post('/admin', authMiddleWare, ReviewController.createAdmin)
+router.post('/user', authMiddleWare, ReviewController.createUser)
 
-router.get('/admin', ReviewController.getAll)
-router.get('/admin/:id', ReviewController.getOne)
-router.get('/user', ReviewController.getAll)
-router.get('/user/:id', ReviewController.getOne)
+router.get('/admin', ReviewController.getAllAdmin)
+router.get('/admin/:id', ReviewController.getOneAdmin)
+router.get('/user', ReviewController.getAllUser)
+router.get('/user/:id', ReviewController.getOneUser)
 
-router.put('/admin/:id', ReviewController.update)
-router.put('/user/:id', ReviewController.update)
+router.put('/admin/:id', ReviewController.updateAdmin)
+router.put('/user/:id', ReviewController.updateUser)
 
-router.delete('/admin/:id', ReviewController.delete)
-router.delete('/user/:id', ReviewController.delete)
+router.delete('/admin/:id', ReviewController.deleteAdmin)
+router.delete('/user/:id', ReviewController.deleteUser)
 
 module.exports = router
